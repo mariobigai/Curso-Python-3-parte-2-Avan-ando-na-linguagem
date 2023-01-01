dbd51feead96ccdc49621c370532b29ba715bc49
Mario Henrique Bigai <109158537+mariobigai@users.noreply.github.com>
2023-01-01 16:10:30 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,18 +1,177 @@
+import random
+from unidecode import unidecode
+
 def jogar():
+    imprime_mensagem_abertura()
+
+    palavra_secreta = carrega_palavra_secreta()
+    palavra_secreta_simplificada = simplifica_palavra_secreta(palavra_secreta)
+
+    letras_acertadas = incializa_letras_acertadas(palavra_secreta)
+    letras_tentadas = ""
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    #enquanto(True)
+    while(not enforcou and not acertou):
+        chute = pede_chute()
+
+        if (chute in letras_tentadas):
+            imprime_mensagem_aviso()
+        else:
+            letras_tentadas += chute
+
+        if (chute in palavra_secreta_simplificada or chute in palavra_secreta):
+            marca_chute_correto(chute, letras_acertadas, palavra_secreta, palavra_secreta_simplificada)
+        else:
+            erros += 1
+            desenha_forca(erros)
+
+
+        enforcou = erros == 7
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+        print("Letras tentadas: {}".format(list(letras_tentadas)))
+
+    if ("_" not in letras_acertadas):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor(palavra_secreta)
+
+    print("Fim do jogo")
+
+def imprime_mensagem_aviso():
+    print("Você já chutou essa letra! Escolha outra letra")
+
+
+def simplifica_palavra_secreta(palavra_secreta):
+    palavra = unidecode(palavra_secreta)
+    return palavra
+
+def desenha_forca(erros):
+    print("Ops, parece que você errou, você tem mais {} tentativas".format(7 - erros))
+    print("  _______     ")
+    print(" |/      |    ")
+
+    if(erros == 1):
+        print(" |      (_)   ")
+        print(" |            ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 2):
+        print(" |      (_)   ")
+        print(" |      \     ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 3):
+        print(" |      (_)   ")
+        print(" |      \|    ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 4):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |            ")
+        print(" |            ")
+
+    if(erros == 5):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |            ")
+
+    if(erros == 6):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      /     ")
+
+    if (erros == 7):
+        print(" |      (_)   ")
+        print(" |      \|/   ")
+        print(" |       |    ")
+        print(" |      / \   ")
+
+    print(" |            ")
+    print("_|___         ")
+    print()
+
+def imprime_mensagem_vencedor():
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
+
+def imprime_mensagem_perdedor(palavra_secreta):
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta, palavra_secreta_simplificada):
+    index = 0
+    for letra in palavra_secreta_simplificada:
+        if (chute == letra):
+            letras_acertadas[index] = palavra_secreta[index]
+        index += 1
+
+
+def imprime_mensagem_abertura():
     print("*********************************")
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
 
-	enforcou = False
-	acertou = False
+def carrega_palavra_secreta():
+    # arquivo = open("palavra.txt", "r")
+    palavras = []
+    with open("palavra.txt", "r") as arquivo:
+        for linha in arquivo:
+            linha = linha.strip()
+            palavras.append(linha)
+    # arquivo.close()
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
-
-    print("Fim do jogo")
+    return palavra_secreta
 
 
-if(__name__ == "__main__"):
+def incializa_letras_acertadas(palavra):
+    return ["-" if letra == "-" else "_" for letra in palavra]
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+
+    return chute
+
+if (__name__ == "__main__"):
     jogar()
--- jogos.py
+++ jogos.py
@@ -1,5 +1,5 @@
 import forca
-import adivinhacao
+import adivinha
 
 def escolhe_jogo():
     print("*********************************")
@@ -10,12 +10,12 @@ def escolhe_jogo():
 
     jogo = int(input("Qual jogo? "))
 
-    if(jogo == 1):
+    if (jogo == 1):
         print("Jogando forca")
         forca.jogar()
-    elif(jogo == 2):
+    elif (jogo == 2):
         print("Jogando adivinhação")
-        adivinhacao.jogar()
+        adivinha.jogar()
 
-if(__name__ == "__main__"):
+if (__name__ == "__main__"):
     escolhe_jogo()
